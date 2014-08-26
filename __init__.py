26c661206ddd75e285f208d99c8d22bbb616ff7d
mholden8 <mholden8@cs.queensu.ca>
2014-08-26 12:06:59 -0400

    Updating PythonMetrics for compatibility with new PythonMetricsCalculator module.

---
+++ __init__.py
@@ -0,0 +1,16 @@
+PerkTutorCoreMetrics = []
+
+import ElapsedTime
+PerkTutorCoreMetrics.append( ElapsedTime.PerkEvaluatorMetric() )
+
+import PathLength
+PerkTutorCoreMetrics.append( PathLength.PerkEvaluatorMetric() )
+
+import TissueDamage
+PerkTutorCoreMetrics.append( TissueDamage.PerkEvaluatorMetric() )
+
+import TissuePath
+PerkTutorCoreMetrics.append( TissuePath.PerkEvaluatorMetric() )
+
+import TissueTime
+PerkTutorCoreMetrics.append( TissueTime.PerkEvaluatorMetric() )
